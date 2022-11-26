dccda9887fc7667ab15641acaf4c5c627595296e
Guilherme Mattoso <guilhermemktfran@gmail.com>
2022-11-26 20:02:29 -0300

    ..

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator/types/decorator/decorators';
+import { IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
